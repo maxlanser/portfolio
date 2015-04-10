9f3b969a9edccfb68f11aeea0d388cdf4a3a6826
Maxim Pogrebnoy <maxlanser@gmail.com>
2015-04-10 13:19:46 +0300

    заготовка

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require("gulp"),
+	connect = require("gulp-connect"),
+	opn = require("opn");
+
+//Запуск локального сервера
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true,
+    port: 8888
+  });
+  opn('http://localhost:8888');
+});
+ 
+//работа с HTML
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+
+//работа с CSS
+gulp.task('css', function () {
+  gulp.src('./app/css/*.css')
+    .pipe(connect.reload());
+});
+
+//слежка за файлами 
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/css/*.css'], ['css']);
+});
+ 
+ //задача по умолчанию
+gulp.task('default', ['connect', 'watch']);
