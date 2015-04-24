06d235d1a354ab8e86e2b820470d5c9525a352a0
Maxim Pogrebnoy <maxlanser@gmail.com>
2015-04-25 01:21:30 +0300

    IE8 - placeholder, Add project page, markup's

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-
-
     // initialize tooltipster on text input elements
     $('#contactme .name input').tooltipster({
         animation: 'grow',
@@ -34,7 +32,54 @@ $(document).ready(function () {
         offsetX: '-4',
         theme: 'tooltipster-portfolio'
     });
-
+    $('#add-project .project-name input').tooltipster({
+        animation: 'grow',
+        trigger: 'custom',
+        onlyOne: false,
+        position: 'left',
+        offsetX: '-4',
+        theme: 'tooltipster-portfolio'
+    });
+    $('#add-project .project-img input').tooltipster({
+        animation: 'grow',
+        trigger: 'custom',
+        onlyOne: false,
+        position: 'left',
+        offsetX: '-4',
+        theme: 'tooltipster-portfolio'
+    });
+     $('#add-project .project-url input').tooltipster({
+        animation: 'grow',
+        trigger: 'custom',
+        onlyOne: false,
+        position: 'left',
+        offsetX: '-4',
+        theme: 'tooltipster-portfolio'
+    });
+      $('#add-project #textarea').tooltipster({
+        animation: 'grow',
+        trigger: 'custom',
+        onlyOne: false,
+        position: 'left',
+        offsetX: '-4',
+        theme: 'tooltipster-portfolio'
+    });
+    $('#authorization .auth-login input').tooltipster({
+        animation: 'grow',
+        trigger: 'custom',
+        onlyOne: false,
+        position: 'left',
+        offsetX: '-4',
+        theme: 'tooltipster-portfolio'
+    });
+    $('#authorization .auth-pass input').tooltipster({
+        animation: 'grow',
+        trigger: 'custom',
+        onlyOne: false,
+        position: 'left',
+        offsetX: '-4',
+        theme: 'tooltipster-portfolio'
+    });
 $("#contactme").validate({
         errorPlacement: function (error, element) {
                 $(element).tooltipster('update', $(error).text());
@@ -60,4 +105,66 @@ $("#contactme").validate({
             return false;
              }
     });
+    $("#add-project").validate({
+        errorPlacement: function (error, element) {
+                $(element).tooltipster('update', $(error).text());
+                $(element).tooltipster('show');
+        },
+        success: function (label, element) {
+            $(element).tooltipster('hide');
+        },
+        rules: {
+            project_name: "required",
+            project_img: "required",
+            project_url: "required",
+            text: "required"
+        },
+        messages: {
+            project_name: "введите название",
+            project_img: "изображение",
+            project_url: "ссылка на проект",
+            text: "описание проекта"
+        },
+        submitHandler: function() { 
+            alert("Все поля заполнены!");
+            return false;
+             }
+    });
+    $('.add').on('click', function(){
+        $('#add-project-popup').bPopup({
+            opacity: 0.75,
+            modalColor: '#58697a',
+            onClose: function() { 
+               $('#add-project .project-name input').tooltipster('hide');
+               $('#add-project .project-img input').tooltipster('hide');
+               $('#add-project .project-url input').tooltipster('hide');
+               $('#add-project #textarea').tooltipster('hide');
+            },
+            onOpen: function() { 
+
+            }
+        });
+    });
+    $("#authorization").validate({
+        errorPlacement: function (error, element) {
+                $(element).tooltipster('update', $(error).text());
+                $(element).tooltipster('show');
+        },
+        success: function (label, element) {
+            $(element).tooltipster('hide');
+        },
+        rules: {
+            login: "required",
+            pass: "required"
+        },
+        messages: {
+            login: "введите логин",
+            pass: "введите пароль"
+        },
+        submitHandler: function() { 
+            alert("Все поля заполнены!");
+            return false;
+             }
+    });
+    $('input, textarea').placeholder();
 });
